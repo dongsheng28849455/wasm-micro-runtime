406cfcf1a67e36f3871e1b2897734fd176698d07
HongxiaWangSSSS <Hongxia.Wang@sony.com>
2023-08-14 11:33:41 +0800

    remove muti-module in WAMRC

--- wamr-compiler/main.c
+++ wamr-compiler/main.c
@@ -103,40 +103,6 @@ unregister_and_unload_native_libs(uint32 native_lib_count,
 }
 #endif
 
-#if WASM_ENABLE_MULTI_MODULE != 0
-static char *module_search_path = ".";
-static bool
-module_reader_callback(const char *module_name, uint8 **p_buffer,
-                       uint32 *p_size)
-{
-    const char *format = "%s/%s.wasm";
-    uint32 sz = (uint32)(strlen(module_search_path) + strlen("/")
-                         + strlen(module_name) + strlen(".wasm") + 1);
-    char *wasm_file_name = BH_MALLOC(sz);
-    if (!wasm_file_name) {
-        return false;
-    }
-
-    snprintf(wasm_file_name, sz, format, module_search_path, module_name);
-    printf("- bh_read_file_to_buffer %s\n", wasm_file_name);
-    *p_buffer = (uint8 *)bh_read_file_to_buffer(wasm_file_name, p_size);
-
-    wasm_runtime_free(wasm_file_name);
-    return *p_buffer != NULL;
-}
-
-static void
-moudle_destroyer(uint8 *buffer, uint32 size)
-{
-    if (!buffer) {
-        return;
-    }
-
-    wasm_runtime_free(buffer);
-    buffer = NULL;
-}
-#endif /* WASM_ENABLE_MULTI_MODULE */
-/* clang-format off */
 static void
 print_help()
 {
@@ -606,9 +572,7 @@ main(int argc, char *argv[])
 #endif
 
     bh_print_time("Begin to load wasm file");
-#if WASM_ENABLE_MULTI_MODULE != 0
-    wasm_runtime_set_module_reader(module_reader_callback, moudle_destroyer);
-#endif
+
     if (!(wasm_file =
               (uint8 *)bh_read_file_to_buffer(wasm_file_name, &wasm_file_size)))
         goto fail1;
